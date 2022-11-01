16ec5539a65d8a76a1655106991e556a9e2f3433
Yalda <afsharyalda@rocketmail.com>
2022-11-01 16:28:44 -0600

    removed console.logs from code

--- src/components/Card-Items.js
+++ src/components/Card-Items.js
@@ -1,11 +1,9 @@
-import CardItem from './CardItem';
+import CardItem from "./CardItem";
 
-const CarDItems = ({showList, navigation}) => {
-  
-  console.log("serached2-->", showList);
-   return showList.map((each, index) => {
-     return <CardItem each={each} index={index} navigation={navigation}  />
-   })
-} 
+const CarDItems = ({ showList, navigation }) => {
+  return showList.map((each, index) => {
+    return <CardItem each={each} index={index} navigation={navigation} />;
+  });
+};
 
 export default CarDItems;
